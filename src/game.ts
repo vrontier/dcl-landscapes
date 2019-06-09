13dea5268f4fec6163173b26445da0bc8777b24b
Mike Quest <development@quest.de>
2019-06-10 01:23:03 +0200

    Update game.ts
    Testing the building system

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,12 @@
 // Building library
-import {createCloud, landScaping, terrain} from "./dcl-landscaping/library"
+import {createCloud, landScaping, terrain} from "./dcl-landscaping/library" 
 
+let building_1_path:string = "models/building/bridge_0001_center_v01.gltf"
+let building_1:Entity = new Entity()
+building_1.addComponent(new GLTFShape(building_1_path))
+building_1.addComponent(new Transform({
+    position: new Vector3(8, 0, 8),
+    scale: new Vector3(1, 1, 1),
+    rotation: Quaternion.Euler(0, 0, 0)
+}))
+engine.addEntity(building_1)
