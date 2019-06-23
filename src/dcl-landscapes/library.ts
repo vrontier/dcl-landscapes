964bebb06d07edcbecb1f6cbde2b851d45580af5
Mike Quest <development@quest.de>
2019-06-23 05:07:23 +0200

    Added animations: rotation and move along path

--- src/dcl-landscapes/library.ts
+++ src/dcl-landscapes/library.ts
@@ -6,6 +6,9 @@
 
 // Master control switch for logging
 let logging: boolean = true
+let msgERROR: string = "DCL Landscapes - ERROR: "
+let msgWARNING: string = "DCL Landscapes - WARNING: "
+let msgDEBUG: string = "DCL Landscapes - DEBUG: "
 
 // Model repository
 let modelDirectory: string = "models/"
@@ -40,21 +43,27 @@ export class Layer {
     private _rawDatapointCountMinimum: number = 2 // minimum raw data be the layer cell coordinate (x,z) and model id
     private _name: string
     private _rawData: string
+    private _dimension: Vector3
     private _position: Vector3
+    private _pivotOffset: Vector3
     private _rotation: Quaternion
     private _cellSize: number
     private _scale: Vector3
     private _entityPositionInLayer: EntityPositionInLayerItem[]
     private _displayLabels: boolean
+    private _layerEntity: Entity
 
-    constructor(name: string, rawData: string, position: Vector3, rotation: Quaternion, cellSize: number, scale: Vector3, labels: boolean) {
+    constructor(name: string, dimension: Vector3, rawData: string, position: Vector3, rotation: Quaternion, cellSize: number, scale: Vector3, labels: boolean) {
         this._name = name
         this._rawData = rawData
+        this._dimension = dimension
         this._position = position
         this._rotation = rotation
         this._cellSize = cellSize
         this._scale = scale
         this._displayLabels = labels
+        //this._pivotOffset = new Vector3( ((dimension.x*cellSize)/2), ((dimension.y*cellSize)/2), ((dimension.z*cellSize)/2) )
+        this._pivotOffset = new Vector3( cellSize/2, 0, cellSize/2 )
 
         // Parse raw data into layer structure
         this._entityPositionInLayer = []
@@ -110,6 +119,20 @@ export class Layer {
         return this._displayLabels
     }
 
+    set entity(newEntity: Entity) {
+        this._layerEntity = newEntity
+    }
+    get entity(): Entity {
+        return this._layerEntity
+    }
+
+    set pivotOffset(newPivotOffset: Vector3) {
+        this._pivotOffset = newPivotOffset
+    }
+    get pivotOffset(): Vector3 {
+        return this._pivotOffset
+    }
+
     get entityArray(): EntityPositionInLayerItem[] {
         return this._entityPositionInLayer
     }
@@ -138,7 +161,7 @@ export class Layer {
             if ( !((rawDataArray[entry].substring(0,1)).match('#')) ) {
                 let rawDataEntryArray: string[] = rawDataArray[entry].split(" ")
                 if (rawDataEntryArray.length < this._rawDatapointCountMinimum) {
-                    log("Warning: entry '" + rawDataArray[entry] + "' does not contain the min. set data points " +
+                    log(msgWARNING + "Entry '" + rawDataArray[entry] + "' does not contain the min. set data points " +
                         "which are layer cell coordinate (x,z) and the model id!")
                 } else {
                     // Parse raw data into structure
@@ -153,11 +176,11 @@ export class Layer {
                     if (rawDataEntryArray[this._rawDatapoint['position']].split(",").length == 2) {
                         entityCellCoordinateInLayer = new Vector3(
                             // relative x coordinate of the cell
-                            parseInt(rawDataEntryArray[this._rawDatapoint['position']].split(",")[0]),
+                            parseInt(rawDataEntryArray[this._rawDatapoint['position']].split(",")[0]) * this._cellSize,
                             // relative y to layer elevation in meter is 0, may be set if elevation is present in raw data
                             0,
                             // relative z coordinate of the cell
-                            parseInt(rawDataEntryArray[this._rawDatapoint['position']].split(",")[1])
+                            parseInt(rawDataEntryArray[this._rawDatapoint['position']].split(",")[1]) * this._cellSize
                         )
                     }
 
@@ -178,6 +201,11 @@ export class Layer {
                     if (rawDataEntryArray.length >= 3) {
                         entityCellCoordinateInLayer.y = parseFloat(rawDataEntryArray[this._rawDatapoint['elevationInLayer']])
                     }
+
+                    // Offset for pivoting
+                    if (logging) log(msgDEBUG + 'Offset ' + entityCellCoordinateInLayer + ' by ' +
+                        entityCellCoordinateInLayer.subtract(this._pivotOffset))
+                    entityCellCoordinateInLayer = entityCellCoordinateInLayer.subtract(this._pivotOffset)
 
                     ////////////////////////////////////////////////////////////////////////////////////////////////////////
                     // Position 4: (optional) rotation
@@ -216,7 +244,6 @@ export class Layer {
                                 parseFloat(rawDataEntryArray[this._rawDatapoint['scale']].split(",")[2])
                             )
                         }
-                        if (logging) log(rawDataEntryArray[this._rawDatapoint['scale']])
                     }
 
                     // Bring it all together in the layer's entity array
@@ -234,6 +261,14 @@ export class Layer {
                             scale: entityScale
                         }
                     )
+
+                    if (logging) log(msgDEBUG +
+                        'Parsed data point: ' + entry + ' "' + rawDataArray[entry] + '" ' +
+                        'with entity id: ' + entityCellCoordinateInLayer +
+                        ', position: ' + entityCellCoordinateInLayer +
+                        ', rotation Euler: ' + entityRotation.eulerAngles +
+                        ', rotation Quaternion: ' + entityRotation +
+                        ', scale: ' + entityScale)
                 }
                 // Increment array index
                 arrayId++
@@ -250,13 +285,14 @@ export class Layer {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export function placeLayer(layer: Layer) {
-    if (logging) log("Placing layer '" + layer.name + "' with " + layer.entityArray.length + " entities.")
+    if (logging) log(msgDEBUG + "Placing layer '" + layer.name + "' with " + layer.entityArray.length + " entitie(s)")
 
     // Source library models
     let entityRepository: EntityRepository = new EntityRepository()
 
     // Create parent layer entity and add it to the system
     let layerParentEntity: Entity = new Entity()
+    layer.entity = layerParentEntity
     engine.addEntity(layerParentEntity)
 
     // Position all entities of a layer
@@ -264,44 +300,68 @@ export function placeLayer(layer: Layer) {
 
         // Assemble entity properties
         let entityFileName: string = modelDirectory + entityRepository.modelFileName(entity.entityId)
-        let entityPosition: Vector3 = new Vector3(
-            entity.position.x * layer.cellSize + layer.cellSize/2 ,
-            entity.position.y,
-            entity.position.z * layer.cellSize + layer.cellSize/2
-        )
+        // let entityPosition: Vector3 = new Vector3(
+        //     entity.position.x * layer.cellSize + layer.cellSize/2 ,
+        //     entity.position.y,
+        //     entity.position.z * layer.cellSize + layer.cellSize/2
+        // )
         let entityScale: Vector3 = entity.scale
         let entityRotation: Quaternion = entity.rotation
-        if (logging) log('Placed entity id ' + entity.entityId + ' (' + entityRepository.modelFileName(entity.entityId) + ') at position ' + entityPosition)
 
         // Create, position, scale and rotate layer child entities
         let childEntity: Entity = new Entity()
         childEntity.setParent(layerParentEntity)
         childEntity.addComponent(new GLTFShape(entityFileName))
         childEntity.addComponent(new Transform({
-            position: entityPosition,
+            position: entity.position,
             scale: entityScale,
             rotation: entityRotation
         }))
 
+        if (logging) log(msgDEBUG +
+            'Added entity id: ' + entity.entityId +
+            ', model file: ' + entityRepository.modelFileName(entity.entityId) +
+            ', position: ' + entity.position +
+            ', rotation Euler: ' + entityRotation.eulerAngles +
+            ', rotation Quaternion: ' + entityRotation +
+            ', scale: ' + entityScale
+        )
+
         if (layer.displayLabels) {
+
+            const letLabelFloatAbove: number = layer.cellSize*2
+            const letLabelRotateZ: number = 90
+            let labelPosition: Vector3 = new Vector3(0, entity.position.y + letLabelFloatAbove, 0)
+            let labelRotation: Quaternion = Quaternion.Euler(0,0,letLabelRotateZ)
+            let labelScale: Vector3 = new Vector3(1, 1, 1)
+
             const childEntityLabel = new Entity()
             const label = new TextShape(
+                '#' + entity.entityId + ': ' +
                 childEntity.getComponent(GLTFShape).src + "\n" +
-                "Position: " + entityPosition + "\n" +
-                "Rotation: (" + entityRotation.eulerAngles.x + ", " + entityRotation.eulerAngles.y+ ", " + entityRotation.eulerAngles.z + ") or " + entityRotation +" \n" +
+                "Position: " + entity.position + "\n" +
+                "Rotation: " + entityRotation.eulerAngles + "\n" +
                 "Scale: " + entityScale
                 )
             childEntityLabel.addComponent(label)
             childEntityLabel.setParent(childEntity)
             childEntityLabel.addComponent(new Transform({
-                position: new Vector3(0, entityPosition.y + 18, 0),
-                scale: new Vector3(1, 1, 1),
-                rotation: Quaternion.Euler(0, 0, 90)
+                position: labelPosition,
+                scale: labelScale,
+                rotation: labelRotation
             }))
             label.fontSize = 14
             label.hTextAlign = "left"
             label.opacity = 1
             label.color = Color3.Black()
+
+            if (logging) log(msgDEBUG +
+                'Added info label at ' +
+                'position: ' + labelPosition +
+                ', rotation Euler: ' + labelRotation.eulerAngles +
+                ' and Quaternion: ' + labelRotation +
+                ', scale: ' + labelScale
+            )
         }
     }
 
@@ -312,7 +372,7 @@ export function placeLayer(layer: Layer) {
         rotation: layer.rotation
     }))
 
-    if (logging) log('Placed parent entity at position ' + layer.position)
+    if (logging) log(msgDEBUG + 'Placed layer parent entity at position ' + layer.position)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -330,37 +390,54 @@ export function landscapeLayer(layer: Layer, positionVariation: Vector3, rotatio
     let turnY: number = 0
     let turnZ: number = 0
     // let turnW: number = 0
-    for (let entity of layer.entityArray) {
 
-        // Let the magic random happen
-        if (positionVariation.x > 0) {
-            moveX = positionVariation.x * ( Math.random() * ((layer.cellSize / 2) - 0) + 0 )
-        }
-        if (positionVariation.z > 0) {
-            moveZ = positionVariation.z * ( Math.random() * ((layer.cellSize / 2) - 0) + 0 )
-        }
-        if (rotationVariation.x > 0) {
-            turnX = entity.rotation.eulerAngles.x + rotationVariation.x * ( Math.random() * (360 - 0) + 0 )
-        }
-        if (rotationVariation.y > 0) {
-            turnY = entity.rotation.eulerAngles.y + rotationVariation.y * ( Math.random() * (360 - 0) + 0 )
-        }
-        if (rotationVariation.z > 0) {
-            turnZ = entity.rotation.eulerAngles.z + rotationVariation.z * ( Math.random() * (360 - 0) + 0 )
-        }
-        // if (rotationVariation.w > 0) {
-        //     turnW = rotationVariation.w * ( Math.random() * (360 - 0) + 0 )
-        // }
+    let noChange: Vector3 = new Vector3(0, 0 ,0)
 
-        // Show old and new position
-        if (logging) {
-            log("Entity id " + entity.id + " position: old=" + entity.position + ", new=" + entity.position.add(new Vector3(moveX, moveY, moveZ)) + ", moveX=" + moveX + ", moveZ=" + moveZ)
-        }
+    if (!positionVariation.equals(noChange) || !rotationVariation.equals(noChange)) {
+        if (logging) log(msgDEBUG+ 'Landscaping layer with position variation: ' + positionVariation + ' and rotation variation: ' + rotationVariation)
+        for (let entity of layer.entityArray) {
 
-        // Update layer entity
-        entity.position = entity.position.add(new Vector3(moveX, moveY, moveZ))
-        entity.rotation = Quaternion.Euler(turnX, turnY, turnZ)
-        layer.updateEntity(entity.id, entity)
+            let positionChange: Vector3 = entity.position
+            let rotationChange: Quaternion = entity.rotation
+
+            // Let the magic random happen
+            if (positionVariation.x > 0) {
+                moveX = positionVariation.x * (Math.random() * ((layer.cellSize / 2) - 0) + 0)
+                positionChange.x = entity.position.x + moveX
+            }
+            if (positionVariation.z > 0) {
+                moveZ = positionVariation.z * (Math.random() * ((layer.cellSize / 2) - 0) + 0)
+                positionChange.y = entity.position.y + moveY
+            }
+            if (rotationVariation.x > 0) {
+                turnX = rotationVariation.x * (Math.random() * (360 - 0) + 0)
+                rotationChange.eulerAngles.x = entity.rotation.eulerAngles.x + turnX
+            }
+            if (rotationVariation.y > 0) {
+                turnY = rotationVariation.y * (Math.random() * (360 - 0) + 0)
+                rotationChange.eulerAngles.y = entity.rotation.eulerAngles.y + turnY
+            }
+            if (rotationVariation.z > 0) {
+                turnZ = rotationVariation.z * (Math.random() * (360 - 0) + 0)
+                rotationChange.eulerAngles.z = entity.rotation.eulerAngles.z + turnZ
+            }
+
+            // New position and rotation
+            if (logging) {
+                log(msgDEBUG +
+                    "Entity id: " + entity.id +
+                    ", old position: " + entity.position +
+                    " and new: " + positionChange +
+                    ", old Euler position (Euler): " + entity.rotation.eulerAngles +
+                    " and new: " + rotationChange.eulerAngles
+                )
+            }
+
+            // Update layer entity
+            entity.position = positionChange
+            entity.rotation = rotationChange
+            layer.updateEntity(entity.id, entity)
+        }
     }
     return layer
 }
